fbc93517711430226ddc926e3ad78f6b0d00f1a9
ParthMmm <parthmangrola@gmail.com>
2022-01-31 22:57:19 -0800

    💄 updated nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Provider } from 'react-redux';
-import { store } from '../app/store';
-import { ApolloProvider } from '@apollo/client';
-import client from '../app/apollo-client';
+
 import theme from '../styles/theme';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import NProgress from 'nprogress'; //nprogress module
-import 'nprogress/nprogress.css'; //styles of nprogress
+// import 'nprogress/nprogress.css'; //styles of nprogress
 import Router from 'next/router';
+
+import 'styles/nprogress.css';
 const queryClient = new QueryClient();
 
 Router.events.on('routeChangeStart', () => NProgress.start());
